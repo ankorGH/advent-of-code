c0388cf6fa159d155ca3356efeed93718cb455d5
Abdul-Harisu Inusah <inusahharis@gmail.com>
2020-12-06 18:22:14 +0000

    fix(puzzle-0): - parse values to numbers - add and multiply unique numbers

--- puzzle-0/index.js
+++ puzzle-0/index.js
@@ -1,15 +1,34 @@
 const readPuzzleInput = require('../line-reader')
 
-//  O(n^3)
-const getMaxPuzzle = async () => {
-  const puzzleInputs = await readPuzzleInput(`${__dirname}/puzzle-input.txt`)
+const getMaxPuzzleForTwo = (inputs) => {
   let maxMul = 0;
 
-  for(let firstNum of puzzleInputs) {
-    for(let secondNum of puzzleInputs) {
-      for(let thirdNum of puzzleInputs) {
-        if( firstNum + secondNum + thirdNum === 2020 && firstNum * secondNum * thirdNum > maxMul) {
-          maxMul = firstNum * secondNum * thirdNum
+  for(let pointerOne = 0; pointerOne < inputs.length - 1; pointerOne++) {
+    for(let pointerTwo = pointerOne + 1; pointerTwo < inputs.length; pointerTwo++) {
+        let inputA = parseInt(inputs[pointerOne])
+        let inputB = parseInt(inputs[pointerTwo])
+
+        if(inputA + inputB === 2020 && inputA * inputB > maxMul) {
+          maxMul = inputA * inputB
+        }
+    }
+  }
+
+  return maxMul
+}
+
+const getMaxPuzzleForThree = (inputs) => {
+  let maxMul = 0;
+
+  for(let pointerOne = 0; pointerOne < inputs.length - 2; pointerOne++) {
+    for(let pointerTwo = pointerOne + 1; pointerTwo < inputs.length - 1; pointerTwo++) { 
+      for(let pointerThree = pointerTwo + 1; pointerThree < inputs.length - 1; pointerThree++) {  
+        let inputA = parseInt(inputs[pointerOne])
+        let inputB = parseInt(inputs[pointerTwo])
+        let inputC = parseInt(inputs[pointerThree])
+
+        if(inputA + inputB + inputC === 2020 && inputA * inputB * inputC > maxMul) {
+          maxMul = inputA * inputB * inputC
         }
       }
     }
@@ -19,5 +38,7 @@ const getMaxPuzzle = async () => {
 }
 
 (async () => {
-  console.log(await getMaxPuzzle())
+  const puzzleInputs = await readPuzzleInput(`${__dirname}/puzzle-input.txt`)
+  console.log(getMaxPuzzleForTwo(puzzleInputs))
+  console.log(getMaxPuzzleForThree(puzzleInputs))
 })()
